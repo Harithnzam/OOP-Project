a4c2d554ed6e037c94461872cf45e579b2235f5b
Harithnzam <108392357+Harithnzam@users.noreply.github.com>
2023-04-03 11:11:34 +0800

    Update BlackChecker.java

--- OOP Group Assignment (Checkers)/OOP Group Assignment (Checkers)/checkers-master/src/checkers/BlackChecker.java
+++ OOP Group Assignment (Checkers)/OOP Group Assignment (Checkers)/checkers-master/src/checkers/BlackChecker.java
@@ -1,67 +1,62 @@
 package checkers;
 
-import javax.swing.ImageIcon; 
+import javax.swing.ImageIcon;
 import java.net.URL;
 
 public class BlackChecker extends CheckerPosition {
-
+   // Constructor for creating a black checker piece with a normal state
    public BlackChecker(Coordinate c) {
       checkerState = new NormalStateBlack();
       position = c;
       value = BLACK_VALUE_NORMAL;
       stringRep = "X";
    }
-     
-   
+
+   // Getter for the color of the checker piece
    public int getColor() {
       return BLACK;
    }
-   
-   
-  
-   
-   
+
+   // Method for making a normal black checker piece a king
    public void makeKing() {
       checkerState = new KingState();
       value = BLACK_VALUE_KING;
       stringRep = "B";
    }
-   
-   
+
+   // Getter for whether the checker piece is a king
    public boolean isKing() {
       return (value == BLACK_VALUE_KING);
    }
-   
-     
+
+   // Getter for whether the checker piece is in the king row
    public boolean kingRow() {
-      return ( (position.get() >= 29) && (position.get() <= 32) );
+      return (position.get() >= 29 && position.get() <= 32);
    }
-   
-   
+
+   // Method for creating a copy of the black checker piece
    public CheckerPosition copy() {
       CheckerPosition newChecker = new BlackChecker(position);
-      if (value == BLACK_VALUE_KING)
+      if (value == BLACK_VALUE_KING) {
          newChecker.makeKing();
+      }
       return newChecker;
-   }          
+   }
 
-    @Override
-    public ImageIcon getIcon() {
-        return  null;
-    }
+   // Getter for the icon of the black checker piece
+   @Override
+   public ImageIcon getIcon() {
+      return null;
+   }
 
+   // Main method that returns 0 and null for each of the methods
+   public static void main(String[] args) {
+      BlackChecker blackChecker = new BlackChecker(new Coordinate(0));
+      System.out.println("getColor(): " + blackChecker.getColor() + ", expected: 1");
+      System.out.println("isKing(): " + blackChecker.isKing() + ", expected: false");
+      System.out.println("kingRow(): " + blackChecker.kingRow() + ", expected: false");
+
+      System.out.println("getIcon(): " + blackChecker.getIcon() + ", expected: null");
+      System.exit(0);
+   }
 }
-
-/* Comments:-
-
-   This is a Java class for representing a black checker in a Checkers game, which extends the CheckerPosition abstract class.
-   The class includes a constructor that initializes the checker's state, position, value, and string representation.
-
-   The class includes methods for getting the checker's color, making it a king, checking if it is a king, and checking if it is in the king row.
-   The copy() method creates a new instance of the BlackChecker class with the same position, and makes it a king if the original checker is a king.
-
-   The getIcon() method returns null and would need to be implemented to return the image of the black checker.
-   Overall, this class appears to be a basic starting point for representing black checkers in a Checkers game,
-   but additional functionality would likely need to be added to fully implement the game.
-
- */
