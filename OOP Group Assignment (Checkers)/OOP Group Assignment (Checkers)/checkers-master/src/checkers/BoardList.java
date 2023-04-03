243b0ac30aa8a363b558e2313f7d809c77262dea
Harithnzam <108392357+Harithnzam@users.noreply.github.com>
2023-04-03 11:21:12 +0800

    Update BoardList.java

--- OOP Group Assignment (Checkers)/OOP Group Assignment (Checkers)/checkers-master/src/checkers/BoardList.java
+++ OOP Group Assignment (Checkers)/OOP Group Assignment (Checkers)/checkers-master/src/checkers/BoardList.java
@@ -1,106 +1,115 @@
 package checkers;
 
-/** 
- * A linked list of boards.
- */
 public class BoardList {
-   private Board boardList;
-   private Board last;
-   private int listSize;
-   
-   
+
+   // Declare private member variables.
+   private Board boardList; // The first board in the list.
+   private Board last; // The last board in the list.
+   private int listSize; // The number of boards in the list.
+
+   // Constructor method for BoardList class.
    public BoardList() {
-      listSize = 0;   
+      listSize = 0;
       boardList = null;
    }
 
-
+   // Add a board to the end of the list.
    public void add(Board b) {
-      if (boardList == null) { // first element.
+      if (boardList == null) { // If the list is empty, set the first and last boards to b.
          boardList = b;
          last = b;
       }
       else {
-         last.setNext(b);     // attach to end of list.
+         last.setNext(b); // Otherwise, attach b to the end of the list.
          last = b;
-      }               
-      listSize++;
-   } 
-   
-   
+      }
+      listSize++; // Increment the size of the list.
+   }
+
+   // Get the number of boards in the list.
    public int size() {
       return listSize;
-   }   
-   
-   
+   }
+
+   // Get the first board in the list.
    public Board first() {
       return boardList;
    }
-   
-   
+
+   // Get the board at the given index in the list.
    public Board get(int index) throws IndexOutOfBoundsException {
       int current = 0;
       Board board = boardList;
-      while (current != index) {
+      while (current != index) { // Traverse the list until the desired index is reached.
          board = board.getNext();
-         if (board == null) throw new IndexOutOfBoundsException(); 
+         if (board == null) throw new IndexOutOfBoundsException(); // Throw an exception if the index is out of bounds.
          current++;
       }
       return board;
    }
-   
-   
+
+   // Get an iterator over the boards in the list.
    public BoardIterator getIterator() {
       return new BoardIterator(this);
    }
-   
-   
+
+   // Find the best board in the list for the given color.
    public Board findBestBoard(int color) {
       BoardIterator iterator = getIterator();
-      Board bestBoard = iterator.next();
-      while (iterator.hasNext()) {
-         if (color == CheckerPosition.WHITE) 
+      Board bestBoard = iterator.next(); // Assume the first board is the best.
+      while (iterator.hasNext()) { // Traverse the list and compare each board to the current best board.
+         if (color == CheckerPosition.WHITE)
             bestBoard = GameSearch.maxBoard(bestBoard, iterator.next());
-         else 
+         else
             bestBoard = GameSearch.minBoard(bestBoard, iterator.next());
       }
-      return bestBoard;   
+      return bestBoard;
    }
 
-   
+   // Convert the list to a string.
    public String toString() {
       BoardIterator iterator = getIterator();
       String s = "BoardList: ";
-      while (iterator.hasNext()) {
+      while (iterator.hasNext()) { // Traverse the list and add each board's string representation to the string.
          s = s + iterator.next().toString();
-         if (iterator.hasNext()) s = s + " , ";
+         if (iterator.hasNext()) s = s + " , "; // Add a comma and space between boards, unless it's the last one.
       }
       return s;
    }
+
+
+   //to check error in this class
+   public static void main(String[] args) {
+
+      // Create some Board objects.
+      Board board1 = new Board();
+      Board board2 = new Board();
+      Board board3 = new Board();
+
+      // Create a BoardList and add the boards to it.
+      BoardList boardList = new BoardList();
+      boardList.add(board1);
+      boardList.add(board2);
+      boardList.add(board3);
+
+      // Print the size of the list and the first board.
+      System.out.println("List size: " + boardList.size());
+      System.out.println("First board: " + boardList.first().toString());
+
+      // Print the string representation of the list.
+      System.out.println(boardList.toString());
+
+      // Get an iterator over the list and print each board.
+      BoardIterator iterator = boardList.getIterator();
+      while (iterator.hasNext()) {
+         Board board = iterator.next();
+         System.out.println(board.toString());
+      }
+
+      // Find the best board for white and print it.
+      Board bestBoard = boardList.findBestBoard(CheckerPosition.WHITE);
+      System.out.println("Best board for white: " + bestBoard.toString());
+   }
 }
 
-/* Comments:-
 
-This is the implementation of a linked list of boards.
-The class has instance variables to keep track of the first and last elements of the list, as well as the size of the list.
-
-The constructor initializes the list size to zero and the boardList to null.
-
-The add method adds a board to the end of the list. If the boardList is empty, the board becomes the first element of the list.
-Otherwise, the board is attached to the end of the list using the setNext method of the last board.
-
-The size method returns the size of the list.
-
-The first method returns the first board in the list.
-
-The get method returns the board at the specified index.
-It iterates through the list until it reaches the specified index, throwing an IndexOutOfBoundsException if the index is out of bounds.
-
-The getIterator method returns a new BoardIterator object for the BoardList.
-
-The findBestBoard method iterates through the list and returns the board with the best evaluation score, based on the color of the current player.
-It uses the maxBoard and minBoard methods from the GameSearch class to compare the evaluation scores of the boards.
-
-The toString method returns a string representation of the BoardList, which consists of a comma-separated list of the string representations of the individual boards in the list.
-
- */
