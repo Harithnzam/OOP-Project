57a165aa27580c5d2aea371d8d611275d43e36cc
Harithnzam <108392357+Harithnzam@users.noreply.github.com>
2023-04-03 11:08:34 +0800

    Update CheckerPosition.java

--- OOP Group Assignment (Checkers)/OOP Group Assignment (Checkers)/checkers-master/src/checkers/CheckerPosition.java
+++ OOP Group Assignment (Checkers)/OOP Group Assignment (Checkers)/checkers-master/src/checkers/CheckerPosition.java
@@ -1,93 +1,92 @@
 package checkers;
- 
-import java.io.Serializable;
-import javax.swing.ImageIcon; 
 
+import java.io.Serializable;
+import javax.swing.ImageIcon;
+
+// This is an abstract class that extends the Position class and implements the Serializable interface.
 public abstract class CheckerPosition extends Position implements Serializable{
-   
+
+   // Constants that define the color and values of checkers.
    public static final int BLACK = 1;
    public static final int WHITE = 2;
    public static final int WHITE_VALUE_NORMAL = 2;
    public static final int BLACK_VALUE_NORMAL = -2;
    public static final int WHITE_VALUE_KING = 3;
    public static final int BLACK_VALUE_KING = -3;
-   
+
+   // Instance variables that represent the state, position, value, and string representation of a checker.
    protected CheckerState checkerState;
    protected Coordinate position;
    protected int value;
    protected String stringRep;
-   
+
+   // Abstract methods that define the behavior of checkers.
    public abstract boolean isKing();
    public abstract int getColor();
    public abstract void makeKing();
    public abstract CheckerPosition copy();
    public abstract boolean kingRow();
    public abstract ImageIcon getIcon();
-   
-   
+
+   // Getter and setter methods for the position and value of a checker.
    public Coordinate getPosition() {
       return position;
-   } 
-   
-   
+   }
+
    public void setPosition(Coordinate c) {
       position = c;
    }
-   
-   
+
    public int getValue() {
       return value;
    }
-   
-   
+
+   // Methods that find valid moves and jumps for a checker on a given board.
    public boolean findValidMoves(MoveList moveList, final Board board) {
-      return checkerState.findValidMoves(this, board, moveList);  
+      return checkerState.findValidMoves(this, board, moveList);
    }
-   
-   
+
    public boolean findValidJumps(MoveList moveList, final Board board) {
-      return checkerState.findValidJumps(this, board, moveList);  
+      return checkerState.findValidJumps(this, board, moveList);
    }
-   
-   
+
+   // Method that returns a string representation of a checker.
    public String toString() {
-      return stringRep;  
+      return stringRep;
    }
-    
+
+   // Main method that creates a new CheckerPosition object for testing purposes.
+   public static void main (String args[]){
+      CheckerPosition cp = new CheckerPosition() {
+         @Override
+         public boolean isKing() {
+            return false;
+         }
+
+         @Override
+         public int getColor() {
+            return 0;
+         }
+
+         @Override
+         public void makeKing() {
+
+         }
+
+         @Override
+         public CheckerPosition copy() {
+            return null;
+         }
+
+         @Override
+         public boolean kingRow() {
+            return false;
+         }
+
+         @Override
+         public ImageIcon getIcon() {
+            return null;
+         }
+      };
+   }
 }
-
-/* Comments:-
-
-This code defines an abstract class called CheckerPosition, which extends another class Position and implements the Serializable interface.
-It contains a number of constants, fields, and abstract methods related to the state and behavior of a checkers game piece.
-
-The class has four constant integers, representing the color of the checkers piece (either black or white), and the value of the piece when it is in a normal state (not a king) for both black and white.
-Additionally, there are two constants representing the value of the piece when it is a king for both black and white.
-
-The class has four fields:
-
-1. checkerState of type CheckerState representing the state of the checkers piece (normal or king)
-2. position of type Coordinate representing the position of the checkers piece on the game board
-3. value of type int representing the value of the checkers piece
-4. stringRep of type String representing a string representation of the checkers piece.
-
-The class also has six abstract methods:
-
-1. isKing(): a boolean method that returns whether the checkers piece is a king.
-2. getColor(): a method that returns the color of the checkers piece (either BLACK or WHITE).
-3. makeKing(): a void method that makes the checkers piece a king.
-4. copy(): an abstract method that returns a copy of the checkers piece.
-5. kingRow(): a boolean method that returns whether the checkers piece is in the king row (the last row for the opposite color).
-6. getIcon(): a method that returns the icon of the checkers piece.
-
-The class also has three methods:
-
-1.1. getPosition(): a method that returns the position of the checkers piece on the game board.
-1.2. setPosition(): a method that sets the position of the checkers piece on the game board.
-
-2.1. findValidMoves(): a method that finds valid moves for the checkers piece and adds them to the specified MoveList. It returns a boolean indicating whether any moves were found.
-2.2. findValidJumps(): a method that finds valid jumps for the checkers piece and adds them to the specified MoveList. It returns a boolean indicating whether any jumps were found.
-
-3. toString(): a method that returns the string representation of the checkers piece.
-
- */
