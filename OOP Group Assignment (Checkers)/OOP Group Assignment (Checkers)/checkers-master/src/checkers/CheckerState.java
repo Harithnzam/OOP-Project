e4e293f70067d496cf8049249e826effdef0fc49
Harithnzam <108392357+Harithnzam@users.noreply.github.com>
2023-04-03 11:10:03 +0800

    Update CheckerState.java

--- OOP Group Assignment (Checkers)/OOP Group Assignment (Checkers)/checkers-master/src/checkers/CheckerState.java
+++ OOP Group Assignment (Checkers)/OOP Group Assignment (Checkers)/checkers-master/src/checkers/CheckerState.java
@@ -1,29 +1,24 @@
 package checkers;
 
-/** 
- * State pattern for the metods findValidMoves and findValidJumps in class Checker.
- * The two methods delegate the methods to a CheckerState object that can either
- * be NormalStateWhite, NormalStateBlack or KingState. This way an checker can
- * change state when it becomes a king.
- */
-public interface CheckerState {    
-   public boolean findValidMoves(CheckerPosition checker, Board board, 
-                              MoveList validMoves);
+// This is an interface that defines the behavior of a checker state.
+public interface CheckerState {
 
-   public boolean findValidJumps(CheckerPosition checker, Board board,
-                              MoveList validJumps);
+   // Methods that find valid moves and jumps for a checker on a given board.
+   public boolean findValidMoves(CheckerPosition checker, Board board, MoveList validMoves);
+   public boolean findValidJumps(CheckerPosition checker, Board board, MoveList validJumps);
+
+   // Main method that creates a new CheckerState object for testing purposes.
+   public static void main (String args[]){
+      CheckerState cs = new CheckerState() {
+         @Override
+         public boolean findValidMoves(CheckerPosition checker, Board board, MoveList validMoves) {
+            return false;
+         }
+
+         @Override
+         public boolean findValidJumps(CheckerPosition checker, Board board, MoveList validJumps) {
+            return false;
+         }
+      };
+   }
 }
-
-/* Comments:-
-
-This is the interface for the State pattern implementation for the Checker class.
-It declares two methods that will be implemented by different concrete classes that will represent different states that a Checker object can be in.
-
-The findValidMoves and findValidJumps methods are used to find all the valid moves and jumps that a checker piece can make on a given Board object.
-The implementation of these methods is delegated to the CheckerState object that is associated with the Checker object.
-
-This allows the Checker object to change its behavior dynamically depending on its current state.
-The concrete classes that implement the CheckerState interface will be NormalStateWhite, NormalStateBlack or KingState.
-These classes will define how the Checker behaves based on its current state.
-
- */
